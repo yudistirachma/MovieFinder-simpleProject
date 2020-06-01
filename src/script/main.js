739a31856993727f9fd67066b48b6764d327b1a7
rahma yudistira <yudistira.anaga@gmail.com>
2020-06-01 15:01:48 +0700

    initial commit

--- src/script/main.js
+++ src/script/main.js
@@ -11,6 +11,7 @@ function main() {
                     <div class="card-body">
                         <h5 class="card-title">${data.Title}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">${data.Year}</h6>
+                        <a href="#" class="card-link see-detail" data-toggle="modal" data-target="#exampleModal" data-id="${data.imdbID}">See Detail</a>
                     </div>
                 </div>
             </div>
@@ -44,13 +45,63 @@ function main() {
     $('#searchButton').on('click', () => {
         movieSearch();
         $('#search-input').val('');
-    })
+    });
+
     $('#search-input').on('keyup', (event) => {
         if (event.which === 13) {
             movieSearch();
             $('#search-input').val('');
         }
-    })
+    });
+
+    $("#movie-list").on('click', '.see-detail', function () {
+        // console.log($(this).data('id'));
+        var key = $(this).data('id');
+
+        function movieId() {
+            // membuat instance dari XMLHttpRequest
+            const xhr = new XMLHttpRequest();
+
+            // menetapkan callback jka fungsi response sukses dan error
+            xhr.onload = function () {
+                const movieJson = JSON.parse(this.responseText);
+                console.log(movieJson);
+                if (movieJson.Response == "True") {
+                    $('.modal-body').html(`
+                     <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="${movieJson.Poster}" class="img-fluid">
+                            </div>
+                            <div class="col-md-8 ml-auto">
+                                <ul class="list-group">
+                                    <li class="list-group-item"><h5 class="card-title">${movieJson.Title}</h5></li>
+                                    <li class="list-group-item"><h6>${movieJson.Released}</h6></li>
+                                    <li class="list-group-item"><h6>${movieJson.Genre}</h6></li>
+                                    <li class="list-group-item"><p class="card-text"></p>${movieJson.Plot}</li>
+                                </ul>
+                            </div>
+                        </div>
+                     </>
+                    `)
+                } else {
+                    alert(movieJson.Error)
+                }
+            };
+
+            xhr.onerror = function (error) {
+                alert(error);
+            };
+
+            // membuat GET resquest dan menetapkan target URL
+            xhr.open("GET", `http://omdbapi.com/?apikey=29a8d624&i=${key}`);
+            // mengirim request
+            xhr.send();
+        }
+        movieId();
+    });
+
+
 
 
 }
